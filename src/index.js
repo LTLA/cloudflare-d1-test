bb361fbb1b7b902666ca5db5750873b5d946c132
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-07 16:28:01 +1100

    Use the anime GIF database as an example.

--- src/index.js
+++ src/index.js
@@ -12,58 +12,134 @@ import { Router } from 'itty-router';
 
 const router = Router();
 
-router.put('/index_project', async (request, env, context) => {
-    let body = await request.json();
-    let project = body.project;
-    let version = body.version;
-    let paths = body.paths;
-
+router.put('/index', async (request, env, context) => {
     // Setting up the tables if they don't already exist.
     let statements = [
+        env.DB.prepare("DROP TABLE IF EXISTS gifs"),
         env.DB.prepare(`
-CREATE TABLE IF NOT EXISTS gypsum_files (
-    id TEXT PRIMARY KEY, 
-    project TEXT NOT NULL,
-    relpath TEXT NOT NULL,
-    version TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS gifs (
+    gif_path TEXT PRIMARY KEY, 
+    show_id TEXT NOT NULL,
+    origin_url TEXT NOT NULL
 )`),
+        env.DB.prepare("DROP TABLE IF EXISTS shows"),
         env.DB.prepare(`
-CREATE TABLE IF NOT EXISTS gypsum_meta_text (
-    id TEXT NOT NULL,
-    field TEXT NOT NULL,
-    contents TEXT NOT NULL,
-    CONSTRAINT fk_id
-        FOREIGN KEY (id)
-        REFERENCES gypsum_files (id)
-        ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS shows (
+    show_id TEXT PRIMARY KEY,
+    show_name TEXT NOT NULL
+)`),
+        env.DB.prepare("DROP TABLE IF EXISTS characters"),
+        env.DB.prepare(`
+CREATE TABLE IF NOT EXISTS characters (
+    character_id TEXT PRIMARY KEY,
+    character_name TEXT NOT NULL
+)`),
+        env.DB.prepare("DROP TABLE IF EXISTS character_show"),
+        env.DB.prepare(`
+CREATE TABLE IF NOT EXISTS character_show (
+    show_id TEXT NOT NULL,
+    character_id TEXT NOT NULL
+)`),
+        env.DB.prepare("DROP TABLE IF EXISTS character_gif"),
+        env.DB.prepare(`
+CREATE TABLE IF NOT EXISTS character_gif (
+    gif_path TEXT NOT NULL,
+    character_id TEXT NOT NULL
+)`),
+        env.DB.prepare("DROP TABLE IF EXISTS gif_sentiment"),
+        env.DB.prepare(`
+CREATE TABLE IF NOT EXISTS gif_sentiment (
+    gif_path TEXT NOT NULL,
+    sentiment TEXT NOT NULL
 )`)
     ];
 
-    // Adding a deletion command to flush existing entries for this project version.
-    statements.push(env.DB.prepare(`
-DELETE FROM gypsum_files 
-WHERE project = ? AND version = ?
-`).bind(project, version)) 
+    // Fetching the prebuilt indices.
+    let gif_res = await fetch("https://github.com/LTLA/acceptable-anime-gifs/releases/download/latest/gifs.json");
+    if (!gif_res.ok) {
+        return new Response({ "error": "failed to retrieve the GIF manifest from GitHub" }, { status: gif_res.status });
+    }
+    let gif_body = await gif_res.json();
 
-    // Indexing the current thing.
-    for (const f of paths) {
-        let id = project + ":" + f.path + "@" + version;
-        console.log(id);
+    let gif_characters = {};
+    let sentiments = [];
+
+    for (const gif of gif_body) {
         statements.push(
-            env.DB.prepare("insert into gypsum_files (id, project, relpath, version) VALUES (?, ?, ?, ?)")
-                .bind(id, project, f.path, version)
+            env.DB.prepare("INSERT into gifs (gif_path, show_id, origin_url) VALUES (?, ?, ?)")
+                .bind(gif.path, gif.show_id, gif.url)
         );
 
-        for (const field of f.text_fields) {
+        for (const s of gif.sentiments) {
             statements.push(
-                env.DB.prepare("insert into gypsum_meta_text (id, field, contents) VALUES (?, ?, ?)")
-                    .bind(id, field.field, field.value)
+                env.DB.prepare("INSERT into gif_sentiment (gif_path, sentiment) VALUES (?, ?)")
+                    .bind(gif.path, s)
+            );
+        }
+
+        for (const c of gif.characters) {
+            if (!(c in gif_characters)) {
+                gif_characters[c] = [];
+            }
+            gif_characters[c].push(gif.path);
+        }
+    }
+
+    let show_res = await fetch("https://github.com/LTLA/acceptable-anime-gifs/releases/download/latest/shows.json");
+    if (!show_res.ok) {
+        return new Response({ "error": "failed to retrieve the show manifest from GitHub" }, { status: show_res.status });
+    }
+    let show_body = await show_res.json();
+
+    let used_character_ids = new Set;
+    let character_to_id = {}
+
+    for (const show of show_body) {
+        statements.push(
+            env.DB.prepare("INSERT into shows (show_id, show_name) VALUES (?, ?)")
+                .bind(show.id, show.name)
+        );
+
+        for (const [k, v] of Object.entries(show.characters)) {
+            if (!used_character_ids.has(v)) {
+                used_character_ids.add(v);
+                statements.push(
+                    env.DB.prepare("INSERT into characters (character_id, character_name) VALUES (?, ?)")
+                        .bind(v, k)
+                );
+            }
+
+            character_to_id[k] = v;
+            statements.push(
+                env.DB.prepare("INSERT into character_show (show_id, character_id) VALUES (?, ?)")
+                    .bind(show.id, v)
+            );
+        }
+    }
+
+    // Inserting the character information.
+    for (const [key, val] of Object.entries(gif_characters)) {
+        if (!(key in character_to_id)) {
+            throw new Response({ "error": "cannot find ID for character '" + key + "'" }, { status: 500 });
+        }
+        let char_id = character_to_id[key];
+
+        for (const vx of val) {
+            statements.push(
+                env.DB.prepare("INSERT into character_gif (gif_path, character_id) VALUES (?, ?)")
+                    .bind(vx, char_id)
             );
         }
     }
 
     let status = await env.DB.batch(statements);
     return new Response(null, { status: 202 });
+});
+
+router.get('/random', async (request, env, context) => {
+    let command = "SELECT gif_path, show_id FROM gifs ORDER BY RANDOM() LIMIT 1";
+    let res = await env.DB.prepare(command).all();
+    return Response.json(res); 
 });
 
 router.get('/search', async (request, env, context) => {
